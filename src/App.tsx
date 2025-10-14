48caa6c2d844786b406abc87c8179f670d212208
[dyad] <arnensiblp@gmail.com>
2025-10-14 10:23:12 +0700

    [dyad] Memperbaiki path impor yang salah - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Layout from "./layouts/Layout";
+import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import MovieDetails from "./pages/MovieDetails";
 import WatchMovie from "./pages/WatchMovie";
